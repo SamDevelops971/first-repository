8b1f667d4ddae1aa8a655f9d3125f0d5eb9916ed
SamDevelops971 <samuelejoseph18@gmail.com>
2025-05-19 18:18:28 -0400

    Update Tic-Tac-Toe.java

--- src/Tic-Tac-Toe.java
+++ src/Tic-Tac-Toe.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class App  {
+public class TicTacToe  {
     public static void main(String[] args) throws Exception {
         char winner = ' ';
         char[] TicTacToe = new char[9];
